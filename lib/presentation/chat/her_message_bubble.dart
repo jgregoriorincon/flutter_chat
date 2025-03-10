bcad57d6fee26efd297cf9debe17a80a71a3e0bf
Jose Gregorio Rincon <jgregoriorincon@gmail.com>
2025-03-10 04:35:30 -0500

    Add image to HerMessageBubble

--- lib/presentation/chat/her_message_bubble.dart
+++ lib/presentation/chat/her_message_bubble.dart
@@ -24,7 +24,24 @@ class HerMessageBubble extends StatelessWidget {
           ),
         ),
         const SizedBox(height: 5),
+        _ImageBubble(),
       ],
+    );
+  }
+}
+
+class _ImageBubble extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
+    return ClipRRect(
+      borderRadius: BorderRadius.circular(20),
+      child: Image.network(
+        'https://yesno.wtf/assets/no/25-55dc62642f92cf4110659b3c80e0d7ec.gif',
+        width: 250,
+        fit: BoxFit.cover,
+      ),
     );
   }
 }
